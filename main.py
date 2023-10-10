816959a0ed37bcfaf785abba0f24d0751ccece48
Kevin <69651491+Kevin-Ponciano@users.noreply.github.com>
2023-10-10 19:09:01 +0000

    add: verficador de palavras

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 from graphviz import Digraph
 
+def verificar_palavras_afd(caminho_arquivo_afd, caminho_arquivo_palavras, caminho_arquivo_saida):
+    # Lê o AFD do arquivo
+    with open(caminho_arquivo_afd, 'r') as arquivo_afd:
+        estados_afd = arquivo_afd.readline().strip().split()
+        estado_inicial_afd = arquivo_afd.readline().strip()
+        estados_finais_afd = set(arquivo_afd.readline().strip().split())
+        transicoes_afd = [linha.strip().split() for linha in arquivo_afd]
+
+    # Função para verificar se uma palavra é aceita pelo AFD
+    def verificar_palavra(palavra):
+        estado_atual = estado_inicial_afd
+        for simbolo in palavra:
+            proximo_estado = None
+            for transicao in transicoes_afd:
+                if transicao[0] == estado_atual and transicao[1] == simbolo:
+                    proximo_estado = transicao[2]
+                    break
+            if proximo_estado is None:
+                return "não aceito"
+            estado_atual = proximo_estado
+        if estado_atual in estados_finais_afd:
+            return "aceito"
+        else:
+            return "não aceito"
+
+    # Verifica as palavras e escreve o resultado no arquivo de saída
+    with open(caminho_arquivo_palavras, 'r') as arquivo_palavras, open(caminho_arquivo_saida, 'w') as arquivo_saida:
+        for palavra in arquivo_palavras:
+            palavra = palavra.strip()
+            resultado = verificar_palavra(palavra)
+            arquivo_saida.write(f"{palavra} {resultado}\n")
+
+
 # Lê um autômato finito não determinístico (AFND) de um arquivo
 def ler_afnd(caminho_arquivo):
     with open(caminho_arquivo, 'r') as arquivo:
@@ -91,3 +124,4 @@ if __name__ == "__main__":
     escrever_afd('saida.txt', estados_afd, estado_inicial_afd, estados_finais_afd, transicoes_afd)
     criar_grafico('afnd', estados, estado_inicial, estados_finais, transicoes)
     criar_grafico('afd', estados_afd, estado_inicial_afd, estados_finais_afd, transicoes_afd)
+    verificar_palavras_afd('saida.txt', 'palavras.txt', 'resultado.txt')
